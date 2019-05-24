e2738d632a70d7ff5f2fde79ed932b2f97c7f9cd
조웅연 <lockhost23@gmail.com>
2019-05-24 23:24:51 +0900

    common 리듀서 추가

--- src/reducer.js
+++ src/reducer.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
 import articleList from './reducers/articleList';
+import common from './reducers/common';
 
 export default combineReducers({
     articleList,
+    common,
 })
