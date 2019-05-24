f035b1a11de74422f05cd5932714bdcbd88c14e7
조웅연 <lockhost23@gmail.com>
2019-05-24 23:26:26 +0900

    del, post 리퀘스트 추가 좋아요 api 추가 태그 api 추가

--- src/api.js
+++ src/api.js
@@ -5,16 +5,24 @@ const api = superagentPromise(superagent, global.Promise);
 const API_ROOT = 'https://conduit.productionready.io/api';
 
 // URL로 데이터를 전달하기 위한 문자열을 인코딩
-const encode = encodeURIComponent;
+/* const encode = encodeURIComponent; */
 const responseBody = res => res.body;
 
 const requests = {
-    get: url => api.get(`${API_ROOT}${url}`).then(responseBody)
+    get: url => api.get(`${API_ROOT}${url}`).then(responseBody),
+    del: url => api.del(`${API_ROOT}${url}`).then(responseBody),
+    post: (url, body) => api.post(`${API_ROOT}${url}`, body).then(responseBody)
 };
 const Articles = {
     all: () => requests.get(`/articles`),
+    favorite: slug => requests.post(`/articles/${slug}/favorite`),
+    unfavorite: slug => requests.del(`/articles/${slug}/favorite`),
+};
+const Tags = {
+    getAll: () => requests.get('/tags')
 };
 
 export default {
     Articles,
+    Tags
 }
