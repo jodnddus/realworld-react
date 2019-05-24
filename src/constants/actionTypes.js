d3be8f21f306e3994e616378e099f53573168aa8
조웅연 <lockhost23@gmail.com>
2019-05-24 23:25:25 +0900

    액션 추가, 리듀서에 액션 추가

--- src/constants/actionTypes.js
+++ src/constants/actionTypes.js
@@ -1,2 +1,9 @@
 // 액션 타입 정의
 export const CHANGE_TAB = 'CHANGE_TAB';
+export const ARTICLE_FAVORITED = 'ARTICLE_FAVORITED';
+export const ARTICLE_UNFAVORITED = 'ARTICLE_UNFAVORITED';
+export const LOGOUT = 'LOGOUT';
+export const LOGIN = 'LOGIN';
+export const REGISTER = 'REGISTER';
+export const HOME_PAGE_LOADED = 'HOME_PAGE_LOADED';
+export const HOME_PAGE_UNLOADED = 'HOME_PAGE_UNLOADED';
--- src/reducers/articleList.js
+++ src/reducers/articleList.js
@@ -1,20 +1,51 @@
 import {
     CHANGE_TAB,
+    ARTICLE_FAVORITED,
+    ARTICLE_UNFAVORITED,
+    HOME_PAGE_LOADED,
+    HOME_PAGE_UNLOADED,
 } from '../constants/actionTypes';
 
 const initialState = {
-    articles: [],
+    articles: [
+        {
+            author: [],
+        }
+    ],
     articlesCount: 0
 }
 
 export default (state = initialState, action) => {
     switch (action.type) {
+        case ARTICLE_FAVORITED:
+        case ARTICLE_UNFAVORITED:
+            console.log(action.payload.article);
+            return {
+                ...state,
+                articles: state.articles.map(article => {
+                    if (article.slug === action.payload.article.slug) {
+                        return {
+                            ...article,
+                            favorite: action.payload.article.favorited,
+                            favoritesCount: action.payload.article.favoritesCount
+                        }
+                    }
+                })
+            }
         case CHANGE_TAB:
             console.log(action.payload);
             return {
                 articles: action.payload.articles,
                 articlesCount: action.payload.articlesCount
             }
+        case HOME_PAGE_LOADED:
+            console.log(action.payload);
+            return {
+                articles: action.payload.articles,
+                articlesCount: action.payload.articlesCount
+            }
+        case HOME_PAGE_UNLOADED:
+            return {};
         default:
             return state;
     }
