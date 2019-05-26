5134fb5827b5acb425aa6a8e10030b80e2526621
조웅연 <lockhost23@gmail.com>
2019-05-26 22:49:03 +0900

    react-redux의 hooks사용 useDispatch, useSelector

--- src/components/Login.js
+++ src/components/Login.js
@@ -6,6 +6,7 @@ import {
     LOGIN,
     LOGIN_PAGE_UNLOADED
 } from '../constants/actionTypes';
+import './Login.css'
 
 /* const mapDispatchToProps = dispatch => ({
     onChangeEmail: value => dispatch({ type: UPDATE_FIELD_AUTH, key: 'email', value }),
@@ -16,13 +17,38 @@ import {
 }) */
 
 function Login() {
+    //mapStateToProps, mapDIspatchToProps 대신 사용하는 react-redux의 hook!
     const authState = useSelector(state => state.auth);
     const authDispatch = useDispatch();
 
+    const emailChange = e => authDispatch({ type: UPDATE_FIELD_AUTH, key: 'email', value: e.target.value })
+    const passwordChange = e => authDispatch({ type: UPDATE_FIELD_AUTH, key: 'password', value: e.target.value });
+    const submitForm = (email, password) => ev => {
+        ev.preventDefault();
+        console.log(1);
+        Promise.resolve(api.Auth.login(email, password)).then((res) => {
+            authDispatch({ type: LOGIN, payload: res });
+        });
+    };
+
     return (
-        <div>
-            <input placeholder="Email" type="email" onChange={e => authDispatch({ type: UPDATE_FIELD_AUTH, key: 'email', value: e.target.value })} />
-            <h1>{authState.email}</h1>
+        <div className="login-page">
+            <h1>Sign In</h1>
+            <form onSubmit={submitForm(authState.email, authState.password)}>
+                <input
+                    className="form-input"
+                    placeholder="Email"
+                    type="email"
+                    onChange={emailChange} />
+                <input
+                    className="form-input"
+                    placeholder="Password"
+                    type="password"
+                    onChange={passwordChange} />
+                <button type="submit">Sign in</button>
+                <h1>{authState.email}{authState.password}</h1>
+            </form>
+            <h1>{authState.errors}</h1>
         </div>
     );
 }
