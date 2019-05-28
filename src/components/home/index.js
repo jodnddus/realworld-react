f5c808969bc91b0d8f26be4de59ee241fc373884
조웅연 <lockhost23@gmail.com>
2019-05-28 21:15:24 +0900

    생활코딩 질문 후 지적사항 반영

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,41 +1,37 @@
 import React from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import { connect } from 'react-redux';
 import FeedTab from './FeedTab';
 import ArticleList from '../ArticleList';
-import Banner from './Banner';
 import api from '../../api';
 import {
     HOME_PAGE_LOADED,
     HOME_PAGE_UNLOADED,
 } from '../../constants/actionTypes';
 
-// const mapStateToProps = state => ({
-//     ...state.articleList,
-//     token: state.auth.token
-// });
+const mapStateToProps = state => ({
+    ...state.articleList,
+    token: state.auth.token
+});
 
-// const mapDispatchToProps = dispatch => ({
-//     onLoad: (tab, pager, payload) => dispatch({ type: HOME_PAGE_LOADED, tab, pager, payload }),
-//     onUnload: () => dispatch({ type: HOME_PAGE_UNLOADED })
-// });
+const mapDispatchToProps = dispatch => ({
+    onLoad: (payload) => dispatch({ type: HOME_PAGE_LOADED, payload }),
+    onUnload: () => dispatch({ type: HOME_PAGE_UNLOADED })
+});
 
-function Home() {
-    const { articleState, tokenState } = useSelector(state => ({ articleState: state.articleList, tokenState: state.auth.token }));
-    const homeDispatch = useDispatch();
-    const tab = tokenState ? 'feed' : 'all';
-    const articlesPromise = tokenState ? api.Articles.feed : api.Articles.all;
-
-    // 동기적으로 실행을 해서 모든 작업을 끝낸 후 render()로 넘어가야 함.a
-    Promise.resolve(api.Articles.all()).then((res) => {
-        homeDispatch({ type: HOME_PAGE_LOADED, tab, articlesPromise, res });
-    });
-
-    return (
-        <div>
-            <FeedTab />
-            <ArticleList articles={articleState.articles} />
-        </div>
-    );
+class Home extends React.Component {
+    componentWillMount() {
+        Promise.resolve(api.Articles.all()).then((res) => {
+            this.props.onLoad(res);
+        });
+    }
+    render() {
+        return (
+            <React.Fragment>
+                <FeedTab />
+                <ArticleList articles={this.props.articles} />
+            </React.Fragment>
+        );
+    }
 }
 
-export default Home;
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
