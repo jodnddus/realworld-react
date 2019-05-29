ab63d9ba71ade3a0a7f82b2db52406e4de9c5361
조웅연 <lockhost23@gmail.com>
2019-05-29 21:13:15 +0900

    common리듀서 추가 & 액션 추가

--- src/reducer.js
+++ src/reducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import articleList from './reducers/articleList';
 import auth from './reducers/auth';
+import common from './reducers/common';
 
 export default combineReducers({
     articleList,
     auth,
+    common,
 })
