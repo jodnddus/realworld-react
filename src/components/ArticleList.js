ce55e740f35140840e05b1407d159583af85b19d
조웅연 <lockhost23@gmail.com>
2019-05-30 20:48:26 +0900

    예외 코드 적용

--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -1,14 +1,26 @@
 import React from 'react';
-import { connect } from 'react-redux';
 import ArticlePreview from './ArticlePreview';
 
+// 프레젠테이셔널 컴포넌트
 const ArticleList = props => {
+    if (!props.articles) {
+        return (
+            <h1>Loading...</h1>
+        );
+    }
+
+    if (props.articles.length === 0) {
+        return (
+            <h1>No articles...</h1>
+        );
+    }
+    
     return (
         <div>
             {
                 props.articles.map(article => {
                     return (
-                        <ArticlePreview article={article} />
+                        <ArticlePreview article={article} key={article.slug}/>
                     );
                 })
             }
