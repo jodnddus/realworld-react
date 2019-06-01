30f10eae32e22ef1e0fc733e5a5777a9a9c7375e
조웅연 <lockhost23@gmail.com>
2019-06-01 16:21:07 +0900

    로그인 토큰을 common 리듀서로 분리함.

--- src/reducers/auth.js
+++ src/reducers/auth.js
@@ -10,12 +10,10 @@ export default (state = {}, action) => {
     switch (action.type) {
         case LOGIN:
         case REGISTER:
-            console.log(action);
             return {
                 ...state,
                 inProgress: false,
                 errors: action.error ? action.payload.errors : null,
-                token: action.error ? null : action.payload.user.token,
             };
         case LOGIN_PAGE_UNLOADED:
         case REGISTER_PAGE_UNLOADED:
