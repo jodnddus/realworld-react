6100d302a505606c195f4cdb33888ec68f6f84ed
조웅연 <lockhost23@gmail.com>
2019-06-03 20:49:25 +0900

    article 리듀서 추가

--- src/reducer.js
+++ src/reducer.js
@@ -2,9 +2,11 @@ import { combineReducers } from 'redux';
 import articleList from './reducers/articleList';
 import auth from './reducers/auth';
 import common from './reducers/common';
+import article from './reducers/article';
 
 export default combineReducers({
     articleList,
     auth,
     common,
+    article
 })
