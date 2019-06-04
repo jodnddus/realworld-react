b93b6ccc80e9e9aa27b051c2e8ba66636e76172a
조웅연 <lockhost23@gmail.com>
2019-06-04 19:42:15 +0900

    deprecated 된 componentWillMount 대신 componentDidMount 사용해서 클래스 컴포넌트를 구성

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,34 +1,30 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import FeedTab from './FeedTab';
-import ArticleList from '../ArticleList';
+import MainView from './MainView';
 import api from '../../api';
 import {
     HOME_PAGE_LOADED,
-    HOME_PAGE_UNLOADED,
 } from '../../constants/actionTypes';
 
 const mapStateToProps = state => ({
-    ...state.articleList,
-    token: state.auth.token
+    token: state.common.token,
 });
 
 const mapDispatchToProps = dispatch => ({
-    onLoad: (payload) => dispatch({ type: HOME_PAGE_LOADED, payload }),
-    onUnload: () => dispatch({ type: HOME_PAGE_UNLOADED })
+    onLoad: (payload) => dispatch({ type: HOME_PAGE_LOADED, payload })
 });
 
 class Home extends React.Component {
-    componentWillMount() {
+    componentDidMount() {
         Promise.resolve(api.Articles.all()).then((res) => {
             this.props.onLoad(res);
         });
     }
+
     render() {
         return (
             <React.Fragment>
-                <FeedTab />
-                <ArticleList articles={this.props.articles} />
+                <MainView />
             </React.Fragment>
         );
     }
