6f30b899ffcc6aadc35bed38407cd72f012fa134
조웅연 <lockhost23@gmail.com>
2019-06-10 11:28:24 +0900

    article 라우터 추가, 앱 로드 코드 추가

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,9 @@ import { BrowserRouter, Route } from 'react-router-dom';
 import Home from '../components/home';
 import Header from './Header';
 import Login from './Login';
+import Article from './Article';
 import api from '../api';
+import './App.css';
 import {
   APP_LOAD,
   REDIRECT,
@@ -15,12 +17,13 @@ const mapStateToProps = state => {
     appLoaded: state.common.appLoaded,
     appName: state.common.appName,
     currentUser: state.common.currentUser,
-    redirectTo: state.common.redirectTo
+    redirectTo: state.common.redirectTo,
+    token: state.common.token
 }};
 
 const mapDispatchToProps = dispatch => ({
   onLoad: (payload, token) =>
-    dispatch({ type: APP_LOAD, payload, token, skipTracking: true }),
+    dispatch({ type: APP_LOAD, payload, token }),
   onRedirect: () =>
     dispatch({ type: REDIRECT })
 });
@@ -33,17 +36,19 @@ class App extends React.Component {
       api.setToken(token);
     }
 
-    this.props.onLoad(token ? api.Auth.current() : null, token);
+    Promise.resolve(api.Auth.current()).then((res) => {
+      this.props.onLoad(res, token);
+    });
   }
 
   render() {
     return (
       <React.Fragment>
         <BrowserRouter>
-          <Header currentUser={this.props.currentUser} />
-          <hr />
+          <Header currentUser={this.props.currentUser} token={this.props.token} />
           <Route exact path="/" component={Home} />
           <Route path="/login" component={Login} />
+          <Route path="/article/:id" component={Article} />
         </BrowserRouter>
       </React.Fragment>
     );
